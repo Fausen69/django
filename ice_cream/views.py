410acad08018e83405bd47183744057ca41cee8f
Fausen69 <daniil.beleaev@gmail.com>
2026-09-15 10:22:06 +0300

    Add files via upload

--- ice_cream/views.py
+++ ice_cream/views.py
@@ -1,8 +1,34 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
+ice_cream_catalog = [
+    {
+        'id': 0,
+        'title': 'Классический пломбир',
+        'description': 'Настоящее мороженое, '
+                       'для истинных ценителей вкуса. '
+                       'Если на столе появляется пломбир'
+                       ' — это не надолго.',
+    },
+    {
+        'id': 1,
+        'title': 'Мороженое с кузнечиками',
+        'description': 'В колумбийском стиле: мороженое '
+                       'с добавлением настоящих карамелизованных кузнечиков.',
+    },
+    {
+        'id': 2,
+        'title': 'Мороженое со вкусом сыра чеддер',
+        'description': 'Вкус настоящего сыра в вафельном стаканчике.',
+    },
+]
 
-def ice_cream_detail(request, pk):
-    return HttpResponse()
+
+def ice_cream_list(request):
+    context = {
+        'ice_cream_catalog': ice_cream_catalog,
+    }
+    return render(request, 'ice_cream/list.html', context)
 
 
 def ice_cream_list(request):
